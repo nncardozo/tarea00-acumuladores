dc2ac474c8ad32573fd4dac844539ff5bf3830c3
nncardozo <ncardozo@campus.ungs.edu.ar>
2025-08-21 00:23:23 -0300

    funciones realizadas
    en este commit se suben las 4 funciones pedidas realizadas donde Junit dio verde en todo.

--- src/acumuladores/Acumuladores.java
+++ src/acumuladores/Acumuladores.java
@@ -14,8 +14,26 @@ public class Acumuladores {
 	 * @return
 	 */
 	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+		if (mat==null||mat.length==0||num<=0){
+			return false;
+		}
+		boolean todosMulNumCol = false;
+		for (int col=0; col<mat[0].length; col++){
+			
+			todosMulNumCol = todosMulNumCol || columnaMultiploNum(mat, col, num);
+		}
+		return todosMulNumCol;
+
+		//		throw new RuntimeException("Metodo no implementado aun!!!");
 	}
+	public boolean columnaMultiploNum(int mat[][],int col, int num){
+		boolean esMultDeNum = true;
+		for (int fila=0; fila<mat.length; fila++){
+			esMultDeNum = esMultDeNum && mat[fila][col]%num==0;
+		}
+		return esMultDeNum;
+	}
+
 	
 	/**
 	 * Dado 2 matrices se verifica si hay intersección entre las filas de cada
@@ -28,10 +46,33 @@ public class Acumuladores {
 	 * @param mat2
 	 * @return
 	 */
-	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
-	}
 	
+	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) { 
+		if (mat2.length==0||mat1.length==0||mat1==null||mat2==null || mat1.length!= mat2.length){
+			return false;
+		}
+		boolean todasFilMult = true;
+		for(int f=0;f<mat1.length;f++) {
+			todasFilMult= todasFilMult && algunaInterPorFila(mat1, mat2, f);
+		}
+		return todasFilMult;
+		
+//		throw new RuntimeException("Metodo no implementado aun!!!");
+	}
+	public boolean hayInterEnUnaFila(int[][] mat, int num,int f){
+		boolean igual=false;
+		for (int c=0;c<mat[0].length;c++){
+			igual=igual||mat[f][c]==num;
+		}
+		return igual;
+	} 
+	public boolean algunaInterPorFila(int[][] mat1, int[][]mat2, int f) { 
+		boolean algunaIntersecFil = false;
+		for(int c=0;c<mat1[0].length;c++){
+			algunaIntersecFil = algunaIntersecFil || hayInterEnUnaFila(mat2, mat1[f][c], f);
+		}
+		return algunaIntersecFil;
+	}
 	/**
 	 * Dada una matriz y el índice de una columna, se verifica si existe alguna
 	 * fila cuya suma de todos sus elementos sea mayor estricto que la suma de
@@ -45,8 +86,34 @@ public class Acumuladores {
 	 * @return
 	 */
 	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+//		throw new RuntimeException("Metodo no implementado aun!!!");
+		if ((mat.length==0 || mat==null)||(nColum<0 || nColum>mat.length)) {
+			return false;
+		}
+		boolean algunaFil = false;
+		for (int f=0; f<mat.length; f++){
+		algunaFil = algunaFil || compara(sumaColumna(mat, nColum), sumaFila(mat, f));
+		}
+		return algunaFil;
 	}
+	public int sumaFila (int[][] mat, int f){
+			int suma = 0;
+			for (int c=0;c<mat[0].length; c++){
+				suma=suma+mat[f][c];
+			}
+			return suma;
+		}
+	public int sumaColumna (int[][] mat, int c){
+		int suma = 0;
+		for (int f=0; f<mat.length; f++){
+			suma=suma+mat[f][c];
+		}
+		return suma;
+	}
+	public boolean compara(int sumaCol, int sumaFil){
+		return sumaCol<sumaFil;
+	}
+
 	
 	/**
 	 * Dadas 2 matrices, se verifica si hay intersección entre las columnas de
@@ -60,6 +127,29 @@ public class Acumuladores {
 	 * @return
 	 */
 	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+//		throw new RuntimeException("Metodo no implementado aun!!!");
+		if(mat1==null || mat2==null || mat1.length==0 || mat2.length==0 ||
+				   mat1[0].length!=mat2[0].length){
+					return false;
+		}
+		boolean interseccionColumna = true;
+		for (int col=0;col<mat1[0].length;col++){
+			interseccionColumna = interseccionColumna && igualNumEnCol(mat1, mat2, col); 
+		}
+		return interseccionColumna;
+	}
+	public boolean igualNumEnCol(int[][] mat1, int[][]mat2, int col) {
+		boolean igual = false;
+		for(int f=0; f<mat1.length; f++){
+			igual = igual || comparar(mat2, mat1[f][col], col);
+		}
+		return igual;
+	}
+	public boolean comparar(int[][] mat, int num, int c){
+		boolean resul = false;
+		for (int f=0; f<mat.length; f++){
+			resul = resul || mat[f][c]==num;
+		}
+		return resul;
 	}
 }
